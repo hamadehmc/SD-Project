0bf5316ac522550357fcd51641b909b53ef02d94
skool <skool@192.168.0.22>
2020-02-11 20:24:43 -0500

    Commit3

--- src/project/Player.java
+++ src/project/Player.java
@@ -17,8 +17,7 @@ public abstract class Player
     
     /**
      * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player..
-     */
+     * @param name the unique ID to assign to this player.
     public Player(String name)
     {
         playerID= name;
